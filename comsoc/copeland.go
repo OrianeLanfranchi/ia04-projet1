3f952bc2b02e524303f070a5182d186e9c9500ba
OrianeLanfranchi <131607571+OrianeLanfranchi@users.noreply.github.com>
2024-11-06 09:31:14 +0100

    [ADD] Copeland rules

--- comsoc/copeland.go
+++ comsoc/copeland.go
@@ -13,5 +13,36 @@ func CopelandSWF(p Profile) (count Count, err error) {
 		count[alt] = 0
 	}
 
+	for alt1 := 1; alt1 < len(p[0])+1; alt1++ {
+		for alt2 := alt1 + 1; alt2 < len(p[0])+1; alt2++ {
+			alt1Preffered, alt2Preffered := 0, 0
+
+			for _, pref := range p {
+				if isPref(Alternative(alt1), Alternative(alt2), pref) {
+					alt1Preffered++
+				} else {
+					alt2Preffered++
+				}
+			}
+
+			if alt1Preffered > alt2Preffered {
+				count[Alternative(alt1)]++
+				count[Alternative(alt2)]--
+			} else if alt1Preffered < alt2Preffered {
+				count[Alternative(alt1)]--
+				count[Alternative(alt2)]++
+			}
+
+		}
+	}
+
 	return count, nil
+}
+
+func CopelandSCF(p Profile) (bestAlts []Alternative, err error) {
+	count, err := CopelandSWF(p)
+	if err != nil {
+		return nil, err
+	}
+	return maxCount(count), err
 }
